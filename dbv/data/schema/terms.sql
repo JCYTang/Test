aa7a47d22e58a373e400dd412ac84c121dba6151
JCYTang <jeremy.cy.tang@gmail.com>
2016-08-14 16:15:58 +1000

    add table

---
+++ dbv/data/schema/terms.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `terms` (
+  `terms_id` int(11) NOT NULL AUTO_INCREMENT,
+  `terms_description` varchar(50) NOT NULL,
+  `terms_due_days` int(11) NOT NULL,
+  PRIMARY KEY (`terms_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
